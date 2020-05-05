c446e774348e6bce3bea576e018fb88da6b0fd72
Markus Durscher <markus.durscher@logisoft.ch>
2020-05-05 23:27:48 +0200

    Added node

--- src/db/Seed.ps1
+++ src/db/Seed.ps1
@@ -1,48 +1,29 @@
+function BuildSql {
+    $sqlScript = "DELETE dbo.Persons;`r`n"
 
-$Header = 'LastName', 'FirstName', 'Sex', 'DateOfBirth', 'DateOfDeath', 'Hometown', 'PlaceOfBirth', 'PlaceOfResidence', 'Profession'
-$P = Import-Csv -Path "./Persons.csv" -Header $Header -Encoding UTF8
-$Sql = "DELETE dbo.Persons;`r`n" `
-    + "INSERT INTO dbo.Persons (LastName,FirstName,Sex,DateOfBirth,DateOfDeath,Hometown,PlaceOfBirth,PlaceOfResidence,Profession) VALUES`r`n"
-$P | ForEach-Object -Process {
-    $Sql = $Sql + "`t(" `
-        + "'" + $_.LastName + "'," `
-        + "'" + $_.FirstName + "'," `
-        + "'" + $_.Sex + "'," `
-        + "'" + $_.DateOfBirth + "',"
-    if ($_.DateOfDeath) {
-        $Sql = $Sql + "'" + $_.DateOfDeath + "',"
+    $personData = Import-Csv -Path "./Persons.csv" -Encoding UTF8
+
+    $personData | ForEach-Object -Process {
+        $lines += "`t($($_.PersonId),$($_.LastName),$($_.FirstName),$($_.Sex),$($_.DateOfBirth),$($_.DateOfDeath),$($_.Hometown),$($_.PlaceOfBirth),$($_.PlaceOfResidence),$($_.Profession)),`r`n"
     }
-    else {
-        $Sql = $Sql + "null,"
+    if ($lines.Length -gt 0) {
+        $sqlScript += "SET IDENTITY_INSERT dbo.Persons ON;`r`n"
+        ForEach ($header in $personData[0].psobject.properties.name) {
+            $columnNames += $header + ","
+        }
+        $sqlScript += "INSERT INTO dbo.Persons ($($columnNames.TrimEnd(','))) VALUES`r`n"
+        ForEach ($line In $lines) {
+            $sqlScript += $line
+        }
+        # Replace the last coma by a semicolon
+        $sqlScript = $sqlScript -replace ",`r`n$", ";`r`n"
+        $sqlScript += "SET IDENTITY_INSERT dbo.Persons OFF;`r`n"
     }
-    if ($_.Hometown) {
-        $Sql = $Sql + "'" + $_.Hometown + "',"
+
+    if ( $sqlScript.EndsWith(",`r`n") ) {
+        $sqlScript = $sqlScript.Substring(0, $sqlScript.Length - 3);
     }
-    else {
-        $Sql = $Sql + "null,"
-    }
-    if ($_.PlaceOfBirth) {
-        $Sql = $Sql + "'" + $_.PlaceOfBirth + "',"
-    }
-    else {
-        $Sql = $Sql + "null,"
-    }
-    if ($_.PlaceOfResidence) {
-        $Sql = $Sql + "'" + $_.PlaceOfResidence + "',"
-    }
-    else {
-        $Sql = $Sql + "null,"
-    }
-    if ($_.Profession) {
-        $Sql = $Sql + "'" + $_.Profession + "'),"
-    }
-    else {
-        $Sql = $Sql + "null),"
-    }
-    $Sql = $Sql + "`r`n"
+    return $sqlScript
 }
 
-if ( $Sql.EndsWith(",`r`n") ) {
-    $Sql = $Sql.Substring(0, $Sql.Length - 3);
-}
-$Sql | Out-File -FilePath "./Seed.sql" -Encoding utf8
+BuildSql | Out-File -FilePath "./Seed.sql" -Encoding utf8
